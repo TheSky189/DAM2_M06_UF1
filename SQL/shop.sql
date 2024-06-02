c60e6dbf7e56d86c95624ccf2b87707c55e61bb2
TheSky189 <113514516+TheSky189@users.noreply.github.com>
2024-06-02 18:39:34 +0200

    Update shop.sql

--- SQL/shop.sql
+++ SQL/shop.sql
@@ -7,10 +7,11 @@ USE shop;
 -- Crear la tabla employee
 CREATE TABLE employee (
     employeeId INT PRIMARY KEY,
+    name VARCHAR(100),
     password VARCHAR(100)
 );
 
 -- Insertar registros de prueba
-INSERT INTO employee (employeeId, password) VALUES (123, 'akali');
-INSERT INTO employee (employeeId, password) VALUES (456, 'zed');
-INSERT INTO employee (employeeId, password) VALUES (789, 'draven');
+INSERT INTO employee (employeeId, name, password) VALUES (123, 'Akali', 'akali');
+INSERT INTO employee (employeeId, name, password) VALUES (456, 'Zed', 'zed');
+INSERT INTO employee (employeeId, name, password) VALUES (789, 'Draven', 'draven');
