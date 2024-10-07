e678b08292a4c6620ad273a1eac9e9850a41d546
TheSky189 <kenkaneki94tg@gmail.com>
2024-10-07 05:43:16 +0200

    Añadir codigos para que pueda implementar con DaoImplFile

--- src/dao/Dao.java
+++ src/dao/Dao.java
@@ -4,6 +4,10 @@ import java.sql.SQLException;
 
 import exception.LimitLoginException;
 import model.Employee;
+import java.util.List;
+import model.Product;
+
+
 
 public interface Dao {
 	// metodo para conectar al base de datos
@@ -15,6 +19,13 @@ public interface Dao {
 	// desconectar base de datos
 	public void disconnect();
 	
+	// --- NUEVAS FUNCIONALIDADES PARA INVENTARIO. v5 ---
 	
+	// metodo para obtener el inventario desde archivo
+	List<Product> getInventory(); // devuelve lista de producto
+	
+	// metodo para escribir el inventario al archivo
+	// devuelve true si esta correcto, false error
+	boolean writeInventory(List<Product> inventory);
 	
 }
